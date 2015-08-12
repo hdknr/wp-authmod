8cc7888c13014c99c74cd9aeb50f112377a776b3
hdknr <gmail@hdknr.com>
2015-08-13 07:55:23 +0900

    resolve hook api method

--- Authmod/App.php
+++ Authmod/App.php
@@ -3,20 +3,32 @@ namespace Authmod;
 
 class App extends Base {
 
-    protected $_filters = ['the_title', 'query_vars'];
-
     function __construct(array $argument = array()) {
-        foreach($this->_filters as $filter){
-            add_filter($filter, array($this, "filter_" . $filter));
-        }
+        $this->_install_hook();
     }      
 
+    function _install_hook(){
+        foreach(get_class_methods($this) as $fn){
+            preg_match("/^(?P<hook>[^_]+)_(?P<name>.+)$/", $fn, $match);
+            if($match != null && 
+               in_array($match['hook'], ['action', 'filter'])){
+                $hook = "add_".$match['hook'];
+                $hook($match['name'], array($this, $fn)); 
+            }   
+        }   
+    }
+
     function filter_the_title($title){
+        error_log("filter:the_title");
         return "$title (debugging)";
     } 
 
     function filter_query_vars($qvars){
-        echo "filter_query_vars......\n";
+        error_log("filter:query_vars");
         return $qvars;
+    }
+
+    function action_init(){
+        error_log("action:init");
     }
 }
