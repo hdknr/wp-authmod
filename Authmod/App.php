638c6cd04de1a14108d0bd34fd77092c62f2cfe6
hdknr <gmail@hdknr.com>
2015-08-13 08:17:40 +0900

    `pre_get_posts` seems to be a good entry point.

--- Authmod/App.php
+++ Authmod/App.php
@@ -25,10 +25,18 @@ class App extends Base {
 
     function filter_query_vars($qvars){
         error_log("filter:query_vars");
-        return $qvars;
+        return $qvars + array('authmod');
     }
 
     function action_init(){
         error_log("action:init");
+    }
+
+    function action_template_redirect(){
+        error_log("action:template_redirect");
+    }
+
+    function action_pre_get_posts(){
+        error_log("action:pre_get_posts");
     }
 }
